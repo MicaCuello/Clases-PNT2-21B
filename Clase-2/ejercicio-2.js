cb652ca35d158ff40ea493d9238255d551ac5a68
MicaCuello <mica23.28214@gmail.com>
2024-08-20 22:22:25 -0300

    MicaCuello

--- Clase-2/ejercicio-2.js
+++ Clase-2/ejercicio-2.js
@@ -1,12 +1,15 @@
 function obtenerNumerosPares(array){
 
+    let pares = array.filter(numero => numero % 2 === 0);
+    return pares;
 
-    return 
 }
 
 
 let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-let pares = obtenerNumerosPares()
+let pares = obtenerNumerosPares(numeros)
 
 console.log(pares);
+
+
--- Clase-2/ejercicio-3.js
+++ Clase-2/ejercicio-3.js
@@ -1,28 +1,40 @@
+// Función que separa las palabras en dos categorías: pares e impares
+function separarPorPares(array) {
 
-// separarPorPalabrasPares
+    // Inicializamos 2 arrays vacíos p/ almacenar longitud de las palabras
+    let resultado = {
+        pares: [],
+        impares: []
+    };
 
-function separarPorPares(){
-
-
-
+    // Iteramos sobre cada palabra del array
     for (let i = 0; i < array.length; i++) {
+        // Guardamos la palabra actual iterada en una variable
+        let palabra = array[i];
         
+        // Verificamos si: longitud es par
+        if (palabra.length % 2 === 0) {
+            resultado.pares.push(palabra);
+
+        } else {
+            // Si es impar: va a array de impares
+            resultado.impares.push(palabra);
+        }
     }
 
-    return resultado
+    // Devolvemos el objeto con palabras separadas
+    return resultado;
 }
 
+// Array (palabras que quiero separar)
+let palabras = ['Auto', 'Parlante', 'Moto', 'Sal', 'Perfume', 'estrella', 'Sol', 'Nubes'];
 
-let palabras = ['Auto', 'Parlante', 'Moto', 'Sal', 'Perfume', 'estrella', 'Sol', 'Nubes']
-
-const resultado = separarPorPares()
+const resultado = separarPorPares(palabras);
 
 console.log(resultado);
 
-
-// Estrucutra del resultado
-
+// La estructura esperada del resultado es:
 // resultado = {
-//     pares:[...],
-//     impares:[...]:
+//     pares: ['Auto', 'Moto', 'Perfume', 'Sol'],
+//     impares: ['Parlante', 'Sal', 'estrella', 'Nubes'] OK 
 // }
